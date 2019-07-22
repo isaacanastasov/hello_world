f673c7f204855a69d057f497d25bdc04c04e8f3b
Isaac Anastasoff <ianastasoff1@gmail.com>
2019-07-22 12:08:05 -0700

    modified main

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+#Step one is to import Jinja2
 import webapp2
+import jinja2
+import logging
+import os
+# Step 2 setup the jinja Environment
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        # logging.info('In helllo handler')
-        self.response.write('<b> hello </b> world <br> This is a great program')
-        self.response.write('<ul> <li> this is a bullet <li> We the best <li> rock paper scisors')
-        self.response.write('</ul>')
-
+        logging.info('Program Ran')
+        # Step 3 Use the jinja Environment to get our file
+        template = jinja_env.get_template('hello.html')
+        self.response.write(template.render())
+        
 class SecretPage(webapp2.RequestHandler):
     def get(self):
         self.response.write('This is the secret entrance to the page')
